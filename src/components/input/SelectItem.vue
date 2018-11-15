b451b8681ea8e86ad0499ee24be719aa84d262aa
bangley <bangley5@gmail.com>
2018-11-15 21:51:21 +0900

    reset

--- src/components/input/SelectItem.vue
+++
@@ -1,7 +0,0 @@
-<script lang="ts" src="./selectitem.ts"></script>
-<template>
-  <v-select :items="options"
-            v-model="model"
-            label="아이템"></v-select>
-</template>
-<style></style>
