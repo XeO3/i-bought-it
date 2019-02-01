45de97723f45e158656afa90e0d5b21b6215d4bf
bangley <bangley5@gmail.com>
2019-02-01 23:33:41 +0900

    home 레이아웃 수정

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,21 @@
 <template>
-<v-container align-center>
-  <v-layout row wrap class="text-xs-center">
-    <v-flex xs12>
-      <img alt="Vue logo" src="../assets/logo.svg" width="400">
-    </v-flex>
-    <v-flex xs12>
-      <h1>
-        이거샀어! I bought it for
-        <b>Whooing</b>
-      </h1>
-      <p>
-        외출시 현금(혹은 교통카드 등)사용 내역을
-        <a :href="hwhooingUrl" target="_blank" rel="noopener">후잉가계부</a>로 재빠르게 입력가능 하도록 도와주는 웹앱입니다.
-      </p>
-    </v-flex>
-  </v-layout>
-</v-container>
-
+  <v-container align-center>
+    <v-layout row wrap class="text-xs-center">
+      <v-flex xs12>
+        <img alt="Vue logo" src="../assets/logo.svg" style="max-height:400px;">
+      </v-flex>
+      <v-flex xs12>
+        <h1>
+          이거샀어! I bought it for
+          <b>Whooing</b>
+        </h1>
+        <p>
+          외출시 현금(혹은 교통카드 등)사용 내역을
+          <a :href="whooingUrl" target="_blank" rel="noopener">후잉가계부</a>로 재빠르게 입력가능 하도록 도와주는 웹앱입니다.
+        </p>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -25,7 +24,7 @@ import Urls from '@/config/Urls';
 
 @Component
 export default class Home extends Vue {
-  get whooingUrl(){
+  get whooingUrl() {
     return Urls.whooing;
   }
 }
