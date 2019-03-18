5ac00fe81db03e7b29ec9830afd2c46cd26f9f41
bangley <bangley5@gmail.com>
2019-03-18 23:55:10 +0900

    입력날짜 버그 수정

--- src/helpers/EntriesInputHelper.ts
+++ src/helpers/EntriesInputHelper.ts
@@ -1,12 +1,12 @@
-import { UserHelper } from '@/store/modules/User';
+import { UserHelper } from "@/store/modules/User";
 import {
   IPostWhooingEntriesData,
-  PostWhooingEntriesData,
-} from '@/models/PostWhooingEntriesData';
-import { postWhooingEntries } from '@/api/PostWhooingEntries';
-import { EntriesModule, AppDataModule } from '@/store/store';
-import { WhooingAccount } from '@/models/EnumWhooingAccount';
-import { WhooingDate } from '@/utils/WhooingDate';
+  PostWhooingEntriesData
+} from "@/models/PostWhooingEntriesData";
+import { postWhooingEntries } from "@/api/PostWhooingEntries";
+import { EntriesModule, AppDataModule } from "@/store/store";
+import { WhooingAccount } from "@/models/EnumWhooingAccount";
+import { WhooingDate } from "@/utils/WhooingDate";
 
 export namespace EntriesInputHelper {
   export async function PushEntryAsync(payload: {
@@ -20,23 +20,24 @@ export namespace EntriesInputHelper {
   }) {
     const data = CreatePostWhooingEntiesData(payload);
     if (!data) {
-      throw Error('거래입력 아이템 생성 실패');
+      throw Error("거래입력 아이템 생성 실패");
     }
     const res = await postWhooingEntries(new PostWhooingEntriesData(data));
     if (!res || res.code !== 200) {
-      throw Error('거래 입력 실패');
+      throw Error("거래 입력 실패");
     }
     const entries = res.results;
     EntriesModule.Push_EntryItem({
       section_id: payload.sId,
-      data: entries,
+      data: entries
     });
 
     entries
       .filter(
-        (item) => Math.floor(item.entry_date) <= WhooingDate.ConvertNumber(new Date()),
+        item =>
+          Math.floor(item.entry_date) <= WhooingDate.ConvertNumber(new Date())
       )
-      .forEach((item) => {
+      .forEach(item => {
         const section_id = payload.sId;
         const money = item.money;
 
@@ -45,7 +46,7 @@ export namespace EntriesInputHelper {
           account: item.l_account,
           account_id: item.l_account_id,
           money,
-          position: 'left',
+          position: "left"
         });
 
         UpdateBalanceState({
@@ -53,7 +54,7 @@ export namespace EntriesInputHelper {
           account: item.r_account,
           account_id: item.r_account_id,
           money,
-          position: 'right',
+          position: "right"
         });
       });
   }
@@ -63,25 +64,25 @@ export namespace EntriesInputHelper {
     account,
     account_id,
     money,
-    position,
+    position
   }: {
     section_id: string;
     account: WhooingAccount;
     account_id: string;
     money: string | number;
-    position: 'left' | 'right';
+    position: "left" | "right";
   }) {
     switch (account) {
       case WhooingAccount.assets:
         AppDataModule.Add_Balance({
           key: { section_id, account_id },
-          addBalance: Number(money) * (position === 'left' ? 1 : -1),
+          addBalance: Number(money) * (position === "left" ? 1 : -1)
         });
         break;
       case WhooingAccount.liabilities:
         AppDataModule.Add_Balance({
           key: { section_id, account_id },
-          addBalance: Number(money) * (position === 'left' ? -1 : 1),
+          addBalance: Number(money) * (position === "left" ? -1 : 1)
         });
         break;
     }
@@ -99,7 +100,7 @@ export namespace EntriesInputHelper {
     const left = UserHelper.GetAccount(payload.sId, payload.left);
     const right = UserHelper.GetAccount(payload.sId, payload.right);
     if (left && right) {
-      return {
+      const result = {
         section_id: payload.sId,
         l_account: left.account,
         l_account_id: left.account_id,
@@ -107,8 +108,12 @@ export namespace EntriesInputHelper {
         r_account_id: right.account_id,
         item: payload.item,
         money: Number(payload.money),
-        memo: payload.memo || '',
+        memo: payload.memo || "",
+        entry_date: payload.date
+          ? WhooingDate.ConvertNumber(payload.date)
+          : undefined,
       };
+      return result;
     }
   }
 }
